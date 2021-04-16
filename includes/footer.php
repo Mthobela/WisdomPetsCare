6daea3de8192cc37caae98d94ef14673dbea29f4
michel muembo <michelmuemboilunga@gmail.com>
2021-04-16 15:45:33 +0200

    wisdom project initialized

---
+++ includes/footer.php
@@ -0,0 +1,5 @@
+this is where you footer will go and remove this comment because it's write as a text
+
+
+</body>
+</html>
---
+++ index.php
@@ -0,0 +1,17 @@
+
+
+// start includes page header
+<?php include 'includes/header.php'?>
+// end includes page header
+
+
+//  start main
+
+
+// end main
+
+
+
+// start includes page footer
+<?php include 'includes/footer.php' ?>
+// end includes page footer
