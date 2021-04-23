74bc0b980bdbb688a37ae0d59b15907199a1689f
michel muembo <michelmuemboilunga@gmail.com>
2021-04-23 16:51:35 +0200

    edit style

--- includes/header.php
+++ includes/header.php
@@ -2,7 +2,7 @@
     <div class="container d-flex align-items-center justify-content-between">
 
         <div class="logo">
-            <h1><a href="index.html">Wisdom Pets</a></h1>
+            <h1><a href="index.php">Wisdom Pets</a></h1>
             <!-- For image logo-->
             <!-- <a href="index.html"><img src="assets/img/logo.png" alt="logo" class="img-fluid"></a>-->
         </div>
@@ -13,17 +13,17 @@
                 <li><a class="nav-link scrollto" href="#about">About Us</a></li>
                 <li class="dropdown"><a href="#services"><span>Services</span> <i class="bi bi-chevron-down"></i></a>
                     <ul>
-                        <li><a href="#">Service 1</a></li>
-                        <li><a href="#">Service 2</a></li>
-                        <li><a href="#">Service 3</a></li>
-                        <li><a href="#">Service 4</a></li>
+                        <li><a href="#">Selling Pets</a></li>
+                        <li><a href="#">Pets Accommodation</a></li>
+                        <li><a href="#">Pets Medical Cares</a></li>
+
                     </ul>
                 </li>
                 <li><a class="nav-link scrollto" href="#team">Team</a></li>
-                <li><a class="nav-link scrollto " href="#portfolio">Gallery</a></li>
+                <li><a class="nav-link scrollto " href="#gallery">Gallery</a></li>
                 <li><a class="nav-link scrollto" href="#contact">Contact</a></li>
-                <li><a class="nav-link scrollto active" id="btnlog" href="#portfolio">Log In</a></li>
-                <li><a class="nav-link scrollto active" id="btnreg" href="#pricing">Register</a></li>
+                <li><a class="nav-link scrollto active" id="btnlog" href="#login">Log In</a></li>
+                <li><a class="nav-link scrollto active" id="btnreg" href="#register">Register</a></li>
             </ul>
             <i class="bi bi-list mobile-nav-toggle"></i>
         </nav>
