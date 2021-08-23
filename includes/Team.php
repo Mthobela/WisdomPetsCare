9f13d0a8a1ec15c5fbe4bdc89d3056d514f92f9d
Kweila <kmthobela12@gmail.com>
2021-08-23 19:31:40 +0200

    Kwanele pushed Team, Testimonials and footer

---
+++ includes/Team.php
@@ -0,0 +1,105 @@
+<section id="team" class="team">
+    <div class="container">
+
+        <div class="section-title" data-aos="zoom-out">
+            <h2>Team</h2>
+            <p>Our Hardworking Team</p>
+        </div>
+
+        <div class="row">
+
+            <div class="col-lg-3 col-md-6 d-flex align-items-stretch">
+                <div class="member" data-aos="fade-up">
+                    <div class="member-img">
+                        <img src="assets/img/teamphotos/team1.jpg" class="img-fluid" alt="">
+                        <div class="social">
+                            <a href=""><i class="bi bi-twitter"></i></a>
+                            <a href=""><i class="bi bi-facebook"></i></a>
+                            <a href=""><i class="bi bi-instagram"></i></a>
+                            <a href=""><i class="bi bi-linkedin"></i></a>
+                        </div>
+                    </div>
+                    <div class="member-info">
+                        <h4>Michel Muembo Ilunga</h4>
+                        <span>Full Stack Developer</span>
+                    </div>
+                </div>
+            </div>
+
+            <div class="col-lg-3 col-md-6 d-flex align-items-stretch">
+                <div class="member" data-aos="fade-up" data-aos-delay="100">
+                    <div class="member-img">
+                        <img src="assets/img/teamphotos/Nana.jpeg" class="img-fluid" alt="">
+                        <div class="social">
+                            <a href=""><i class="bi bi-twitter"></i></a>
+                            <a href=""><i class="bi bi-facebook"></i></a>
+                            <a href=""><i class="bi bi-instagram"></i></a>
+                            <a href=""><i class="bi bi-linkedin"></i></a>
+                        </div>
+                    </div>
+                    <div class="member-info">
+                        <h4>Nana</h4>
+                        <span>Web Designer</span>
+                    </div>
+                </div>
+            </div>
+
+            <div class="col-lg-3 col-md-6 d-flex align-items-stretch">
+                <div class="member" data-aos="fade-up" data-aos-delay="200">
+                    <div class="member-img">
+                        <img src="assets/img/teamphotos/siya.jpeg" class="img-fluid" alt="">
+                        <div class="social">
+                            <a href=""><i class="bi bi-twitter"></i></a>
+                            <a href=""><i class="bi bi-facebook"></i></a>
+                            <a href=""><i class="bi bi-instagram"></i></a>
+                            <a href=""><i class="bi bi-linkedin"></i></a>
+                        </div>
+                    </div>
+                    <div class="member-info">
+                        <h4>Siyanda</h4>
+                        <span>Back End Developer</span>
+                    </div>
+                </div>
+            </div>
+
+            <div class="col-lg-3 col-md-6 d-flex align-items-stretch">
+                <div class="member" data-aos="fade-up" data-aos-delay="300">
+                    <div class="member-img">
+                        <img src="assets/img/teamphotos/kwanele.jpeg" class="img-fluid" alt="">
+                        <div class="social">
+                            <a href=""><i class="bi bi-twitter"></i></a>
+                            <a href=""><i class="bi bi-facebook"></i></a>
+                            <a href=""><i class="bi bi-instagram"></i></a>
+                            <a href=""><i class="bi bi-linkedin"></i></a>
+                        </div>
+                    </div>
+                    <div class="member-info">
+                        <h4>Kwanele</h4>
+                        <span>Front End Developer</span>
+                    </div>
+                </div>
+            </div>
+
+            <div class="col-lg-3 col-md-6 d-flex align-items-stretch">
+                <div class="member" data-aos="fade-up" data-aos-delay="400">
+                    <div class="member-img">
+                        <img src="assets/img/teamphotos/viwe.jpeg" class="img-fluid" alt="">
+                        <div class="social">
+                            <a href=""><i class="bi bi-twitter"></i></a>
+                            <a href=""><i class="bi bi-facebook"></i></a>
+                            <a href=""><i class="bi bi-instagram"></i></a>
+                            <a href=""><i class="bi bi-linkedin"></i></a>
+                        </div>
+                    </div>
+                    <div class="member-info">
+                        <h4>Viwe</h4>
+                        <span>Web Developer</span>
+                    </div>
+                </div>
+            </div>
+
+        </div>
+
+    </div>
+</section>
+
---
+++ includes/testimonials.php
@@ -0,0 +1,87 @@
+
+<section id="testimonials" class="testimonials">
+    <div class="container">
+
+        <div class="section-title" data-aos="zoom-out">
+            <h2>Testimonials</h2>
+            <p>What they are saying about us</p>
+        </div>
+
+        <div class="testimonials-slider swiper-container" data-aos="fade-up" data-aos-delay="100">
+            <div class="swiper-wrapper">
+
+                <div class="swiper-slide">
+                    <div class="testimonial-item">
+                        <p>
+                            <i class="bx bxs-quote-alt-left quote-icon-left"></i>
+                            Registering with Wisdom's Pet was easy; everyone was professional and thorough. The CEO Michel, my pet sitter, was extremely detail-oriented.
+                            <i class="bx bxs-quote-alt-right quote-icon-right"></i>
+                        </p>
+                        <img src="assets/img/testimonialspictures/test1.jpg" class="testimonial-img" alt="">
+                        <h3>Yves Mwembo</h3>
+                        <h4>Ceo &amp; Founder</h4>
+                    </div>
+                </div><!-- End testimonial item -->
+
+                <div class="swiper-slide">
+                    <div class="testimonial-item">
+                        <p>
+                            <i class="bx bxs-quote-alt-left quote-icon-left"></i>
+                            Hi, I just wanted to let you know how blown away I am with your company and your customer service. I head up customer service for a Federal agency so I always recognize and call out truly outstanding customer service when I see it. My experience with your company and our dog walker Campagna is hands down the best I have ever found. We are so, so happy We found Wisdom Pets.
+                            <i class="bx bxs-quote-alt-right quote-icon-right"></i>
+                        </p>
+                        <img src="assets/img/testimonialspictures/test2.jpg" class="testimonial-img" alt="">
+                        <h3>Divine Losa</h3>
+                        <h4>Designer</h4>
+                    </div>
+                </div><!-- End testimonial item -->
+
+                <div class="swiper-slide">
+                    <div class="testimonial-item">
+                        <p>
+                            <i class="bx bxs-quote-alt-left quote-icon-left"></i>
+                            We had never used a pet sitting service before, but we will always use Wisdom Pets in the future.
+                            <i class="bx bxs-quote-alt-right quote-icon-right"></i>
+                        </p>
+                        <img src="assets/img/testimonialspictures/test3.jpg" class="testimonial-img" alt="">
+                        <h3>Dieu Merci Kasongo</h3>
+                        <h4>Store Owner</h4>
+                    </div>
+                </div><!-- End testimonial item -->
+
+                <div class="swiper-slide">
+                    <div class="testimonial-item">
+                        <p>
+                            <i class="bx bxs-quote-alt-left quote-icon-left"></i>
+                            Exactly what I wanted and needed.
+                            I have had Labradors all my life. But I have never had dogs like the ones from Wisdom Pets <strong style="color: red;">❤❤</strong>  🐕‍.
+                            <i class="bx bxs-quote-alt-right quote-icon-right"></i>
+                        </p>
+                        <img src="assets/img/testimonialspictures/test4.jpeg" class="testimonial-img" alt="">
+                        <h3>Stella Ntumba</h3>
+                        <h4>Entrepreneur</h4>
+                    </div>
+                </div><!-- End testimonial item -->
+
+                <div class="swiper-slide">
+                    <div class="testimonial-item">
+                        <p>
+                            <i class="bx bxs-quote-alt-left quote-icon-left"></i>
+                            This is the only breeder I would trust!
+                            For anyone interested in a high-quality purebred labrador retriever, this is the only breeder I would trust! The Stokes dedication to producing offspring that represents the best characteristics of labs, from conformation to working ability to temperament, is unbeatable. They are also dedicated to performing every test they can on their breeding animals to ensure they do not pass on congenital/genetic diseases in their lines.<br>
+                            Best of The Best wisdom Pets.
+                            <i class="bx bxs-quote-alt-right quote-icon-right"></i>
+                        </p>
+                        <img src="assets/img/testimonialspictures/test5.jpeg" class="testimonial-img" alt="">
+                        <h3>Cyrille Mulumba</h3>
+                        <h4>Doctor</h4>
+                    </div>
+                </div><!-- End testimonial item -->
+
+            </div>
+            <div class="swiper-pagination"></div>
+        </div>
+
+    </div>
+</section>
+
